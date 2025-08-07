f64929e842ff5ce16828ba6716b2b9968cd77efc
Charles9412 <carlos_hierro@hotmail.com>
2025-08-07 14:19:30 -0600

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="mls_utils",
-    version="0.1.0",
-    description="Helper functions for MLS scoring and segmentation",
-    author="Carlos Eugenio Miranda Rocha",
+    name="mls-utils",
+    version="0.1",
     packages=find_packages(),
-    install_requires=[
-        "pandas",
-        "numpy",
-        "scikit-learn",
-        "matplotlib",
-        "seaborn",
-        "gradio"
-    ],
+    install_requires=[],
 )
+
